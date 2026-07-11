bd8b53a048894660573699ebec6e869b1b8748ff
Sane1204 <vermasanskar698@gmail.com>
2026-07-11 13:11:57 +0100

    Time: 0 ms (100.00%) | Memory: 20.4 MB (40.83%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,16 +1,18 @@
 class Solution:
-    ##submitting previous leetcode
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        lol={}
         ans = []
+        left = 0 
+        lol={}
+
         for i in range(len(nums)):
             key = target - nums[i]
             if key in lol:
-                ans= [lol[key], i]
+                ans = [lol[key],i]
                 break
-
-            lol[nums[i]] =i
+            
+            lol[nums[i]] = i
         return ans
 
+
         
+
